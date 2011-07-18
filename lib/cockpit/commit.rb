edeb10a7938259c74889bb9dc6cd74f7572ceefa
Corey Collins <corey@corey-ubuntu.(none)>
2011-07-18 12:52:07 -0500

    added comments

--- lib/cockpit/commit.rb
+++ lib/cockpit/commit.rb
@@ -3,13 +3,35 @@ module Cockpit
 
     attr_accessor :sha, :message, :parents, :author, :url, :tree, :committer
 
+    # Find a commit
+    #
+    #   examples:
+    #     Blob.find('cockpit', '45f6d8e74e145b910be4e15f67ef3892fe7abb26')
     def self.find(repo, sha, user=nil)
       raise AuthenticationRequired unless Api.authenticated
       user ||= User.current.login
       Commit.new(get("/repos/#{user}/#{repo}/commits/#{sha}"))
     end
 
-    def self.create(repo, message, tree, parents, options={})
+    # Create a commit
+    #
+    # Options:
+    #  :author
+    #     :name
+    #     :date
+    #     :email
+    #  :committer
+    #     :name
+    #     :date
+    #     :email
+    #
+    #   examples:
+    #     Commit.create('cockpit', 'This is a new commit', '45f6d8e74e145b910be4e15f67ef3892fe7abb26', ['45f6d8e74e145b910be4e15f67ef3892fe7abb26'])
+    #     
+    #     Commit.create('cockpit', 'Comit with author', '45f6d8e74e145b910be4e15f67ef3892fe7abb26', ['45f6d8e74e145b910be4e15f67ef3892fe7abb26'], {
+    #       :author => {:name => 'Corey', :email => 'corey@gitpilot.com'}
+    #     })
+    def self.create(repo, message, tree, parents=[], options={})
       raise AuthenticationRequired unless Api.authenticated
       params = {:message => message, :tree => tree, :parents => parents}.merge(options)
       user = User.current.login
--- lib/cockpit/tag.rb
+++ lib/cockpit/tag.rb
@@ -2,12 +2,32 @@ module Cockpit
   class Tag < Base
     attr_accessor :tag, :sha, :message, :tagger, :object, :url
 
+    # Find a tag
+    #
+    #   examples:
+    #     Tag.find('cockpit', '45f6d8e74e145b910be4e15f67ef3892fe7abb26')
     def self.find(repo, sha, user=nil)
       raise AuthenticationRequired unless Api.authenticated
       user ||= User.current.login
       Tag.new(get("/repos/#{user}/#{repo}/git/tags/#{sha}"))
     end
 
+    # Create a tag
+    #
+    #
+    # Options:
+    #  :type,    values: 'commit', 'tree', and 'blob'. default is commit
+    #  :tagger
+    #     :name
+    #     :date
+    #     :email
+    #
+    #   examples:
+    #     Tag.create('cockpit', v1.0', 'New tag', '45f6d8e74e145b910be4e15f67ef3892fe7abb26')
+    #     
+    #     Commit.create('cockpit', v1.0', 'New tag', '45f6d8e74e145b910be4e15f67ef3892fe7abb26', {
+    #       :tagger => {:name => 'Corey', :email => 'corey@gitpilot.com'}
+    #     })
     def self.create(repo, tag, message, object, options={})
       raise AuthenticationRequired unless Api.authenticated
       options = {:type => 'commit'}.merge(options)
